cff0382f0721e0d1beb406853a1723cc5d4b204b
pe.chanaka.ck <pe.chanaka.ck@1148ddae-d9e1-8bae-d2e9-17b7c9359dd7>
2011-07-15 10:42:17 +0000

    test for service method using moc-objects

--- trunk/test/ProjectServiceTest.php
+++ trunk/test/ProjectServiceTest.php
@@ -1,137 +1,38 @@
 <?php
 
 require_once 'PHPUnit/Framework.php';
+require_once sfConfig::get('sf_test_dir') . '/util/TestDataService.php';
 
 class ProjectServiceTest extends PHPUnit_Framework_TestCase {
 
-    public function testTrackProjectProgress() {
-
-
-//        $projectService = new ProjectService();
-//        $projectService->trackProjectProgress('2011-2-2', "ACCEPTED", 1);
-//        $projectProgress = $this->_getProjectProgress(1, '2011-2-2');
-//        $this->assertEquals(10,$projectProgress[0]->getWorkCompleted());
-//
-//        $projectService->trackProjectProgress('2011-1-25', "PENDING", 2);
-//        $projectProgress = $this->_getProjectProgress(1, '2011-1-25');
-//        $this->assertEquals(0,$projectProgress[0]->getWorkCompleted());
-//
-//        $projectService->trackProjectProgress('2011-2-15', "ACCEPTED", 3);
-//        $projectProgress = $this->_getProjectProgress(1, '2011-2-15');
-//        $this->assertEquals(15,$projectProgress[0]->getWorkCompleted());
-//
-//        $projectService->trackProjectProgress('2011-1-25', "ACCEPTED", 4);
-//        $projectProgress = $this->_getProjectProgress(1, '2011-1-25');
-//        $this->assertEquals(40,$projectProgress[0]->getWorkCompleted());
-//
-//        $projectService->trackProjectProgressAddStory('2010-6-12', "ACCEPTED", 1, "100");
-//        $projectProgress = $this->_getProjectProgress(1, '2010-6-12');
-//        $this->assertEquals(100,$projectProgress[0]->getWorkCompleted());
-//
-//        $projectService->trackProjectProgressAddStory('2010-6-22', "PENDING", 1, "100");
-//        $projectProgress = $this->_getProjectProgress(1, '2010-6-22');
-//        $this->assertEquals(null,$projectProgress[0]->getWorkCompleted());
-
-    }
+    protected $fixture;
+    protected $projectService;
 
     public function setup() {
 
-        $this->deleteTestData();
-        $this->insertTestData();
-        
+        $this->fixture = sfConfig::get('sf_test_dir') . '/fixtures/ProjectService.yml';
+        $this->projectService = new ProjectService();
     }
 
-    public function deleteTestData() {
+    public function testGetProjectsByUser() {
+
+        $projectList = TestDataService::loadObjectList('Project', $this->fixture, 'set1');
+
+        $ProjectDao = $this->getMock('ProjectDao', array('getProjectsByUser'));
+        $ProjectDao->expects($this->once())
+                   ->method('getProjectsByUser')
+                   ->with(2)
+                   ->will($this->returnValue($projectList));
+
+        $this->projectService->setProjectDao($ProjectDao);
+
+        $returnedProjectList = $this->projectService->getProjectsByUser(2);
+
+        $this->assertEquals(3, count($returnedProjectList));
         
-        $projectQuery = Doctrine_Query::create()->delete()->from('Project')->execute();
-        $storyQuery = Doctrine_Query::create()->delete()->from('Story')->execute();
-        $projectProgressQuery = Doctrine_Query::create()->delete()->from('ProjectProgress')->execute();
-        
+        foreach ($returnedProjectList as $returnedProject) {
+            $this->assertTrue($returnedProject instanceof Project);
+        }
     }
 
-
-     public function _getProjectProgress($projectId, $acceptedDate) {
-
-        $query = Doctrine_Core::getTable('ProjectProgress')
-                        ->createQuery('c')
-                        ->where('c.project_id = ?', $projectId)
-                        ->andWhere('c.accepted_date= ?', $acceptedDate);
-        return $query->execute();
-
-    }
-
-    public function insertTestData() {
-
-        $project = new Project();
-        $project->setId(1);
-        $project->setName("OrangePM");
-        $project->save();
-
-        $story = new Story();
-        $story->setId(1);
-        $story->setProjectId(1);
-        $story->setEstimation(10);
-        $story->setName('story1');
-        $story->setDateAdded('2011-1-12');
-        $story->setStatus('Pending');
-        $story->setDeleted(Story::FLAG_ACTIVE);
-        $story->save();
-
-        $story = new Story();
-        $story->setId(2);
-        $story->setProjectId(1);
-        $story->setEstimation(5);
-        $story->setName('story2');
-        $story->setDateAdded('2011-1-13');
-        $story->setStatus('Accepted');
-        $story->setAcceptedDate('2011-1-25');
-        $story->setDeleted(Story::FLAG_ACTIVE);
-        $story->save();
-
-        $projectProgress = new ProjectProgress();
-        $projectProgress->setProjectId(1);
-        $projectProgress->setAcceptedDate('2011-1-25');
-        $projectProgress->setWorkCompleted(5);
-        $projectProgress->setUnitOfWork(2);
-        $projectProgress->save();
-
-        $story = new Story();
-        $story->setId(3);
-        $story->setProjectId(1);
-        $story->setEstimation(15);
-        $story->setName('story3');
-        $story->setDateAdded('2011-1-14');
-        $story->setStatus('Accepted');
-        $story->setAcceptedDate('2011-1-27');
-        $story->setDeleted(Story::FLAG_ACTIVE);
-        $story->save();
-
-        $projectProgress = new ProjectProgress();
-        $projectProgress->setProjectId(1);
-        $projectProgress->setAcceptedDate('2011-1-27');
-        $projectProgress->setWorkCompleted(15);
-        $projectProgress->setUnitOfWork(2);
-        $projectProgress->save();
-        $story = new Story();
-        
-        $story->setId(4);
-        $story->setProjectId(1);
-        $story->setEstimation(40);
-        $story->setName('story4');
-        $story->setDateAdded('2011-1-14');
-        $story->setStatus('Accepted');
-        $story->setAcceptedDate('2011-1-30');
-        $story->setDeleted(Story::FLAG_ACTIVE);
-        $story->save();
-
-        $projectProgress = new ProjectProgress();
-        $projectProgress->setProjectId(1);
-        $projectProgress->setAcceptedDate('2011-1-30');
-        $projectProgress->setWorkCompleted(40);
-        $projectProgress->setUnitOfWork(2);
-        $projectProgress->save();
-        
-    }
-
-    
 }
