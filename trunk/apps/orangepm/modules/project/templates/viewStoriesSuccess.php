2a8dff19619f0c01b1bb64d83885203aa762561d
155indiran@gmail.com <155indiran@gmail.com@1148ddae-d9e1-8bae-d2e9-17b7c9359dd7>
2011-12-22 18:03:48 +0000

    Modified the success classes

--- trunk/apps/orangepm/modules/project/templates/viewStoriesSuccess.php
+++ trunk/apps/orangepm/modules/project/templates/viewStoriesSuccess.php
@@ -9,42 +9,43 @@
 
 <div class="Project">
     <div class="heading">
-        <h3> <?php echo link_to(__('Projects'),'project/viewProjects'); ?> > <?php echo $projectName; ?> > <?php echo __('Stories'); ?> </h3>
+        <h3> <?php echo link_to(__('Projects'),'project/viewProjects'); ?> > <?php echo link_to(__($projectName),"project/viewProjectDetails?projectName={$projectName}&projectId={$id}"); ?> > <?php echo __('Stories'); ?> </h3>
         <span id="message"><?php echo $sf_user->getFlash('addStory') ?></span>
         <span id="noRecordMessage"><?php if(isset($noRecordMessage)) echo $noRecordMessage; ?></span>
     </div>
 
     <div id="mainErrorDiv"></div>
-
-    <table class="tableContent">
-        <tr><td class="pageNav" colspan="7"><?php echo pager_navigation($storyList, url_for("project/viewStories") . "?id={$projectId}&projectName={$projectName}") ?></td></tr>
-        <tr>
-            <th><?php echo __('Story Name') ?></th>
-            <th><?php echo __('Estimated Effort'); ?> <br> <?php echo __('(Engineering Hours)'); ?></th>
-            <th><?php echo __('Date Added') ?></th>
-            <th><?php echo 'Status' ?></th>
-            <th><?php echo 'Accepted Date' ?></th>
-            <th colspan="2"><?php echo __('Actions') ?></th>
-        </tr>
-        <?php if(count($storyList) != 0): ?>
-        <?php foreach ($storyList->getResults() as $story): ?>
-            <tr id="row">
-                <td class="<?php echo "changedName name " . $story->getId(); ?>"><?php echo $story->getName(); ?></td>
-                <td class="<?php echo "changedEstimation estimation " . $story->getId(); ?>"> <?php echo $story->getEstimation(); ?></td>
-                <td class="<?php echo "changedDate date " . $story->getId(); ?>"> <?php echo $story->getDateAdded(); ?></td>
-                <td class="<?php echo "changedStatus status " . $story->getId(); ?>"> <?php echo $story->getStatus(); ?></td>
-                <td class="<?php echo "changedAcceptedDate acceptedDate " . $story->getId(); ?>"> <?php echo $story->getAcceptedDate(); ?></td>
-                <td class="<?php echo "edit edit " . $story->getId(); ?>"><?php echo image_tag('b_edit.png', 'id=editBtn') ?></td>
-                <td class="close"><a class="confirmLink" href="<?php echo url_for("project/deleteStory?id={$story->getId()}&projectId={$projectId}&projectName={$projectName}"); ?>"><?php echo image_tag('b_drop.png'); ?></a></td>
-
+    <div class="StoryShowForm">
+        <table class="tableContent">
+            <tr><td class="pageNav" colspan="7"><?php echo pager_navigation($storyList, url_for("project/viewStories") . "?id={$projectId}&projectName={$projectName}") ?></td></tr>
+            <tr>
+                <th><?php echo __('Story Name') ?></th>
+                <th><?php echo __('Effort');?><?php echo "<span class='moreStar'>*</span>"?></th>
+                <th><?php echo __('Date Added') ?></th>
+                <th><?php echo 'Status' ?></th>
+                <th><?php echo 'Accepted Date' ?></th>
+                <th colspan="2"><?php echo __('Actions') ?></th>
             </tr>
-           
-        <?php endforeach; ?>
-        <?php else: ?>
-            <!-- do not delete the space between <td> tags -->
-            <tr><td> </td><td></td><td></td><td></td><td></td><td></td></tr>
-        <?php endif; ?>
-        </table>
+            <?php if(count($storyList) != 0): ?>
+            <?php foreach ($storyList->getResults() as $story): ?>
+                <tr id="row">
+                    <td class="<?php echo "changedName name " . $story->getId(); ?>"><?php echo $story->getName(); ?></td>
+                    <td class="<?php echo "changedEstimation estimation " . $story->getId(); ?>"> <?php echo $story->getEstimation(); ?></td>
+                    <td class="<?php echo "changedDate date " . $story->getId(); ?>"> <?php echo $story->getDateAdded(); ?></td>
+                    <td class="<?php echo "changedStatus status " . $story->getId(); ?>"> <?php echo $story->getStatus(); ?></td>
+                    <td class="<?php echo "changedAcceptedDate acceptedDate " . $story->getId(); ?>"> <?php echo $story->getAcceptedDate(); ?></td>
+                    <td class="<?php echo "edit edit " . $story->getId(); ?>"><?php echo image_tag('b_edit.png', 'id=editBtn') ?></td>
+                    <td class="close"><a class="confirmLink" href="<?php echo url_for("project/deleteStory?id={$story->getId()}&projectId={$projectId}&projectName={$projectName}"); ?>"><?php echo image_tag('b_drop.png'); ?></a></td>
+    
+                </tr>
+               
+            <?php endforeach; ?>
+            <?php else: ?>
+                <!-- do not delete the space between <td> tags -->
+                <tr><td> </td><td></td><td></td><td></td><td></td><td></td></tr>
+            <?php endif; ?>
+            </table>
+        </div>
         <div class="addButton">
             <form action="<?php echo url_for("project/addStory?id={$projectId}&projectName={$projectName}"); ?>" method="GET">
                 <input type="submit" value="<?php echo __('Add') ?>" />
@@ -55,6 +56,7 @@
                 <input type="submit" value="<?php echo "View Weekly Progress" ?>"/>
         </form>
     </div>
+    <div id="moreField">Estimated Effort (Engineering Hours) <span class="moreStar">*</span></div>
 </div>
 
 
@@ -64,5 +66,4 @@
     <input type="text" name="deletedDate" id="deletedDate" onclick="datepicker()" value="<?php echo Date('Y-m-d')?>" />
 
 </div>
-
 <?php echo javascript_include_tag('viewStories'); ?>
