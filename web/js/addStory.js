ac93e5fb9e95a966ce9c315dfc30ee892b8da9ce
Thilanka Kaushalya <lgtkaushalya@gmail.com>
2011-12-28 08:38:33 +0000

    Fixed the current date selection issue of Add Story

--- web/js/addStory.js
+++ web/js/addStory.js
@@ -1,51 +1,44 @@
 $(document).ready(
     function() {
         document.getElementById('project_acceptedDate').disabled=true;
-        $( "#project_dateAdded" ).datepicker(
+        document.getElementById('project_dateAdded').value = getCurrentDate();
+        $( "#project_dateAdded" ).datepicker({
+            dateFormat: 'yy-mm-dd',
+            changeMonth: true,
+            changeYear: true,
+            showAnim: "slideDown"
+        });
+    }
+);
 
-        {
-                dateFormat: 'yy-mm-dd',
-                changeMonth: true,
-                changeYear: true,
-                showAnim: "slideDown"
-                
-            });
-
-    });
-
-    function clicked(dropdown){
-        //alert(dropdown.selectedIndex);
-
+function clicked(dropdown){
     if(dropdown.selectedIndex==6){
-        //alert("Came Here");
        document.getElementById('project_acceptedDate').disabled=false;
-       var currentTime = new Date()
-        var month = currentTime.getMonth() + 1
-        var day = currentTime.getDate()
-        var year = currentTime.getFullYear()
-        //document.write(month + "/" + day + "/" + year)
 
-       document.getElementById('project_acceptedDate').value = year+"-"+month+"-"+day;
-       $( " #project_acceptedDate" ).datepicker(
 
-        {
+       document.getElementById('project_acceptedDate').value = getCurrentDate();
+       $( " #project_acceptedDate" ).datepicker({
                 dateFormat: 'yy-mm-dd',
                 changeMonth: true,
                 changeYear: true,
                 showAnim: "slideDown"
                
-            });
-    
+       });
     }
     else {
        document.getElementById('project_acceptedDate').value = '';
        document.getElementById('project_acceptedDate').disabled=true;
+    }
+}
 
-    }
-    }
-    
 function passProjectId($projectId, $projectName){
-
     location.href=linkUrl+"?id="+$projectId+"&projectName="+$projectName
-    
+}
+
+function getCurrentDate() {
+    var currentTime = new Date()
+    var month = currentTime.getMonth() + 1
+    var day = currentTime.getDate()
+    var year = currentTime.getFullYear()
+    return year+"-"+month+"-"+day;
 }
