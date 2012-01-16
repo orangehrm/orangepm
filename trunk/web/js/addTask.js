803579aece8af654d27298454720147b1934d6e5
lgtkaushalya@gmail.com <lgtkaushalya@gmail.com@1148ddae-d9e1-8bae-d2e9-17b7c9359dd7>
2012-01-16 05:47:32 +0000

    Fixed cancel button not working issue in task add form

--- trunk/web/js/addTask.js
+++ trunk/web/js/addTask.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
+    $('#cancel').click(function(){  
+        location.href=storyUrl;
+    
+    });
+	
     $("#addTaskForm").validate({
         
         rules: {
