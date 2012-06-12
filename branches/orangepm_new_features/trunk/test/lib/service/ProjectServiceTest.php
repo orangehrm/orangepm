e9f0aefe95ed858ca9c0d8cb924c440171a18511
eranga@orangehrm.us.com <eranga@orangehrm.us.com@1148ddae-d9e1-8bae-d2e9-17b7c9359dd7>
2012-06-12 03:24:43 +0000

    added functions saveProjects(), updateProjects(), getAllProjectsForUser() with test cases

--- branches/orangepm_new_features/trunk/test/lib/service/ProjectServiceTest.php
+++ branches/orangepm_new_features/trunk/test/lib/service/ProjectServiceTest.php
@@ -34,5 +34,73 @@ class ProjectServiceTest extends PHPUnit_Framework_TestCase {
             $this->assertTrue($returnedProject instanceof Project);
         }
     }
+    
+    
+    /**
+     * @author Eranga
+     * Testing weather user is getting saved when passed through service layer
+     */
+    public function testSaveProjectForService(){
+        $project = new Project();
+        $project->setId(11);
+        $project->setName('aaa');
+        $project->setUserId(2);
+        $project->setStartDate('2011-01-01');
+        $projectuser=new ProjectUser();
+        $collection=new Doctrine_Collection('ProjectUser');
+        $projectuser->setProjectId(11);
+        $projectuser->setUserId(2);        
+        $projectuser->setUserType(1);
+        $collection->add($projectuser);
+        $project->setProjectUser($collection);
+        $projectDao = $this->getMock('ProjectDao');
+        $projectDao->expects($this->once())
+            ->method('saveProject')
+            ->with($project);
+        $this->projectService->setProjectDao($projectDao);
+        $this->projectService->saveProject($project);   
+    }
+    
+    /**
+     * @author Eranga
+     * Testing weather user is getting updated when passed through service layer
+     */
+    public function testUpdateProjectForService(){
+        $project = new Project();
+        $project->setId(11);
+        $project->setName('aaa');
+        $project->setUserId(2);
+        $project->setStartDate('2011-01-01');
+        $projectuser=new ProjectUser();
+        $collection=new Doctrine_Collection('ProjectUser');
+        $projectuser->setProjectId(11);
+        $projectuser->setUserId(2);        
+        $projectuser->setUserType(1);
+        $collection->add($projectuser);
+        $project->setProjectUser($collection);
+        $projectDao = $this->getMock('ProjectDao');
+        $projectDao->expects($this->once())
+            ->method('updateProject')
+            ->with($project);
+        $this->projectService->setProjectDao($projectDao);
+        $this->projectService->updateProject($project);   
+    }
+    
+    /**
+     * @author Eranga
+     * Testing weather users can be fetched from ProjectUser table for a given user
+     */
+    public function testGetProjectUsersByUser() {
+        $ProjectDao = $this->getMock('ProjectDao', array('getProjectUsersByUser'));
+        $ProjectDao->expects($this->once())
+                   ->method('getProjectUsersByUser')
+                   ->with(1)
+                   ->will($this->returnValue(array(new ProjectUser())));
+        $this->projectService->setProjectDao($ProjectDao);
+
+        $returnedProjectList = $this->projectService->getProjectUsersByUser(1);
+        $this->assertEquals(1, count($returnedProjectList));
+     
+    }
 
 }
