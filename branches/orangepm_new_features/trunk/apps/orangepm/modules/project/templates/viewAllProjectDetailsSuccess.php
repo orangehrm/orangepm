a16ff401ae316d7097fcc674f69ed0747f0c24ec
gurutharshan@orangehrm.us.com <gurutharshan@orangehrm.us.com@1148ddae-d9e1-8bae-d2e9-17b7c9359dd7>
2012-06-20 11:32:00 +0000

    Guru: added template for project summary

--- branches/orangepm_new_features/trunk/apps/orangepm/modules/project/templates/viewAllProjectDetailsSuccess.php
+++ branches/orangepm_new_features/trunk/apps/orangepm/modules/project/templates/viewAllProjectDetailsSuccess.php
@@ -1,17 +1,52 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of viewAllProjectDetailsSuccess
- *
- * @author guruce
- */
-class viewAllProjectDetailsSuccess {
-    
-}
-
-?>
+<?php echo stylesheet_tag('viewProjectDetails') ?>
+<div class="Project">
+    <div class="heading">
+        <h3> </h3>
+    </div>
+    <?php foreach ($projectProgressList  as $single): ?>
+    <div class="showForm">
+        <div class="headlineField"><?php echo $single['project']->getName(); ?></div>
+        <div class="formField">
+            <div class="form_devision_heading">Details</div>
+                <table class='showTable'>
+                    <tbody>
+                        <tr>
+                            <td><label for="project_projectAdmin"><?php echo __('Project Admin : ') ?></label></td> <td><?php echo $single['project']->getUser()->getFirstName().' '.$single['project']->getUser()->getLastName(); ?> </td>
+                        </tr>
+                        <tr>
+                            <td><label for="project_startDate"><?php echo __('Start Date : ') ?></label></td> <td><?php echo $single['project']->getStartDate(); ?> </td>
+                        </tr>
+                        <tr>
+                            <td><label for="project_endDate"><?php echo __('Start Date : ') ?></label></td> <td><?php echo $single['project']->getStartDate(); ?> </td>
+                        </tr>
+                    </tbody>
+                </table>
+        </div>        
+    </div>
+    <div class="showForm">
+        <div class='formField'>
+            <div class="form_devision_heading">Status</div>
+            <div class='division_content'>
+                <table class='showTable'>
+                    <tbody>
+                        <tr>
+                            <th>Project Status</th>
+                            <th class="percentageColumn">Percentage</th>
+                            <th class="effortColumn"> Effort (<?php echo $storyEstimationCount?>)</th>
+                        </tr>
+                        <?php foreach ($single as $status => $effort): ?>
+                        <?php if($status!='project') { ?>
+                        <tr>
+                           <td><?php echo __($status);?></td>
+                           <td><div id="progressbar_<?php echo str_replace(" ","_",strtolower($status));?>"class="progressbar" value="<?php echo $projectService->getPecentage($effort, $storyEstimationCount)?>"></div></td>
+                           <td><?php echo $effort?></td>
+                        </tr>
+                        <?php } ?>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>        
+    </div>
+    <?php endforeach; ?>
+</div>
